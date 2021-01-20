c13012f4e95cf0039547867027f6df26e73a28e3
gustavo <gustavosoares.nh@gmail.com>
2021-01-20 10:58:10 -0200

    Configure ALLOWED_HOSTS

--- eventex/settings.py
+++ eventex/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-from decouple import config
-from dj_database_url import  parse as dburl
+from decouple import config,Csv
+from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='', cast=Csv())
 
 
 # Application definition
--- eventex/wsgi.py
+++ eventex/wsgi.py
@@ -11,7 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eventex.settings")
 
 application = Cling(get_wsgi_application())
